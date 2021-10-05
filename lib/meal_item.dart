2a311e7530f816e587a949b166be458fa97c9316
summaiya14 <summaiyamahmod@gmail.com>
2021-10-05 22:22:56 +0500

    Filters

--- lib/meal_item.dart
+++ lib/meal_item.dart
@@ -11,6 +11,7 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
+
   MealItem({
     @required this.id,
     @required this.title,
@@ -53,7 +54,12 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext ctx){
-    Navigator.of(ctx).pushNamed(MealDetailScreen.routeName, arguments: id,);
+    Navigator.of(ctx).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: id,
+    ).then((result) {
+      print(result);
+    });
   }
 
   @override
